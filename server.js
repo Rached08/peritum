6a92ded6285bfef244e41d9ce2187c073bd240f8
rached <rached.khemakhem@esprit.tn>
2021-02-22 23:58:56 +0100

    initial chatbot

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ function initial() {
 
 /******************************* */
     
-app.post("/echo", function(req, res) {
+/*app.post("/echo", function(req, res) {
     var speech =
       req.body.queryResult &&
       req.body.queryResult.parameters &&
@@ -262,4 +262,49 @@ app.post("/echo", function(req, res) {
       }
     });
   });
-  
+  */
+
+ const dialogflow = require('@google-cloud/dialogflow');
+ const uuid = require('uuid');
+ 
+ /**
+  * Send a query to the dialogflow agent, and return the query result.
+  * @param {string} projectId The project to be used
+  */
+ async function runSample(projectId = 'peritumbot-uvbc') {
+   // A unique identifier for the given session
+   const sessionId = uuid.v4();
+ 
+   // Create a new session
+   const sessionClient = new dialogflow.SessionsClient({
+       keyFilename:"D:/Users/EVADAM/Desktop/backend-Mohamed/peritumbot-uvbc-4051f4e9b03c.json"
+   });
+   const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
+ 
+   // The text query request.
+   const request = {
+     session: sessionPath,
+     queryInput: {
+       text: {
+         // The query to send to the dialogflow agent
+         text: 'I want to learn flutter',
+         // The language used by the client (en-US)
+         languageCode: 'en-US',
+       },
+     },
+   };
+ 
+   // Send request and log result
+   const responses = await sessionClient.detectIntent(request);
+   console.log('Detected intent');
+   const result = responses[0].queryResult;
+   console.log(`  Query: ${result.queryText}`);
+   console.log(`  Response: ${result.fulfillmentText}`);
+   if (result.intent) {
+     console.log(`  Intent: ${result.intent.displayName}`);
+   } else {
+     console.log(`  No intent matched.`);
+   }
+ }
+ 
+ runSample()
