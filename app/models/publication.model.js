008ee54880428c36a0d5920a40d6b3585f99aba9
rached <rached.khemakhem@esprit.tn>
2021-02-22 22:02:06 +0100

    first push

---
+++ app/models/publication.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+
+const Publication = mongoose.model(
+"Publication",
+new mongoose.Schema({
+    comments:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
+        }
+    ],
+    likes:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+        ref: "Like"
+        }
+    ],
+    author:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+})
+);
+module.exports = Publication;
